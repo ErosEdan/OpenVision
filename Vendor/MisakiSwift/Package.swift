9f4621d3a1a86b8674e22c248ac54c4c2e7a8573
ErosEdan <seedtosausage@gmail.com>
2026-08-12 20:51:57 -0400

    Package vendored model resources for iOS signing

--- Vendor/MisakiSwift/Package.swift
+++ Vendor/MisakiSwift/Package.swift
@@ -27,7 +27,7 @@ let package = Package(
         .product(name: "MLXUtilsLibrary", package: "MLXUtilsLibrary")
      ],
      resources: [
-      .copy("../../Resources/")
+      .process("../../Resources/")
      ]
     )
   ]
